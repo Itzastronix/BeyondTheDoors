5157247a347e681cd8a806b3fd6a4b65a12c18ea
Astronix <astronix.gamedev@gmail.com>
2025-10-05 17:18:15 +0530

    final commit

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,26 +1,60 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+
 public class MainMenu : MonoBehaviour
 {
+    [Header("Scene Settings")]
+    [Tooltip("Name of the scene to load when Play is pressed.")]
+    public string gameSceneName;
 
-    [SerializeField] private Button PlayBtton;
-    [SerializeField] private Button SettingsButton;
-    [SerializeField] private Button CreditsButton;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("UI Panels")]
+    [Tooltip("Credits panel UI.")]
+    public GameObject creditsPanel;
+
+    /// <summary>
+    /// Called when Play button is clicked
+    /// </summary>
+    public void PlayGame()
     {
-
+        Debug.Log("Play button pressed!");
+        if (!string.IsNullOrEmpty(gameSceneName))
+        {
+            SceneManager.LoadScene(gameSceneName);
+        }
+        else
+        {
+            Debug.LogWarning("Game scene name is empty! Assign it in the Inspector.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Called when Credits button is clicked
+    /// </summary>
+    public void ShowCredits()
     {
-        PlayBtton.onClick.AddListener(OnPlayButtonClick);
+        if (creditsPanel != null)
+            creditsPanel.SetActive(true);
     }
 
-    void OnPlayButtonClick()
+    /// <summary>
+    /// Called when Back button in credits is clicked
+    /// </summary>
+    public void HideCredits()
     {
-        SceneManager.LoadScene("MainLevel", LoadSceneMode.Additive);
+        if (creditsPanel != null)
+            creditsPanel.SetActive(false);
+    }
+
+    /// <summary>
+    /// Called when Quit button is clicked
+    /// </summary>
+    public void QuitGame()
+    {
+        Debug.Log("Quit button pressed!");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Stops play mode in Editor
+#else
+        Application.Quit(); // Quits the build
+#endif
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,29 +1,44 @@
 using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float playerSpeed;
-    [SerializeField] private float mouseSenstivity;
+    [Header("Movement Settings")]
+    [SerializeField] private float playerSpeed = 5f;
+    [SerializeField] private float crouchSpeed = 2f;
+    [SerializeField] private float mouseSensitivity = 150f;
+    [SerializeField] private float cameraSmoothTime = 8f;
+
+    [Header("Crouch Settings")]
+    [SerializeField] private float crouchHeight = 0.8f;
+    [SerializeField] private float standHeight = 1.6f;
+    [SerializeField] private float crouchTransitionSpeed = 6f;
+    [SerializeField] private KeyCode crouchKey = KeyCode.LeftControl;
+
+    [Header("References")]
+    [SerializeField] private Transform playerCamera;
+
     private Rigidbody rb;
-    private float RotationX = 0f;
-    [SerializeField] Transform playerCamera;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float rotationX = 0f;
+    private bool isCrouching = false;
+    private float targetHeight;
+    private float heightVelocity = 0f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
         rb.freezeRotation = true;
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
 
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        targetHeight = standHeight;
     }
-    // Update is called once per frame
+
     void Update()
     {
-        CameraMovement();
-
+        HandleCameraMovement();
+        HandleCrouch();
     }
 
     void FixedUpdate()
@@ -31,27 +46,41 @@ public class PlayerMovement : MonoBehaviour
         HandlePlayerMovement();
     }
 
-    void HandlePlayerMovement()
+    private void HandlePlayerMovement()
     {
-        float Moveforward = Input.GetAxis("Vertical");
-        float MoveRight = Input.GetAxis("Horizontal");
+        float moveForward = Input.GetAxis("Vertical");
+        float moveRight = Input.GetAxis("Horizontal");
 
-        Vector3 moveDir = (transform.forward * Moveforward + transform.right * MoveRight).normalized;
-        rb.linearVelocity = new Vector3(moveDir.x * playerSpeed, rb.linearVelocity.y, moveDir.z * playerSpeed);
+        float currentSpeed = isCrouching ? crouchSpeed : playerSpeed;
+
+        Vector3 moveDir = (transform.forward * moveForward + transform.right * moveRight).normalized;
+        rb.linearVelocity = new Vector3(moveDir.x * currentSpeed, rb.linearVelocity.y, moveDir.z * currentSpeed);
     }
 
-    void CameraMovement()
+    private void HandleCameraMovement()
     {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        float MouseX = Input.GetAxis("Mouse X") * mouseSenstivity * Time.deltaTime;
-        float MouseY = Input.GetAxis("Mouse Y") * mouseSenstivity * Time.deltaTime;
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
 
-        RotationX -= MouseY;
-        RotationX = Math.Clamp(RotationX, -90f, 90f);
+        Quaternion targetRotation = Quaternion.Euler(rotationX, 0f, 0f);
+        playerCamera.localRotation = Quaternion.Slerp(playerCamera.localRotation, targetRotation, cameraSmoothTime * Time.deltaTime);
 
-        playerCamera.localRotation = Quaternion.Euler(RotationX, 0f, 0f);
-
-        transform.Rotate(Vector3.up * MouseX);
+        transform.Rotate(Vector3.up * mouseX);
     }
 
+    private void HandleCrouch()
+    {
+        if (Input.GetKeyDown(crouchKey))
+        {
+            isCrouching = !isCrouching;
+            targetHeight = isCrouching ? crouchHeight : standHeight;
+        }
+
+        Vector3 camPos = playerCamera.localPosition;
+        camPos.y = Mathf.SmoothDamp(camPos.y, targetHeight, ref heightVelocity, 1f / crouchTransitionSpeed);
+        playerCamera.localPosition = camPos;
+    }
 }
